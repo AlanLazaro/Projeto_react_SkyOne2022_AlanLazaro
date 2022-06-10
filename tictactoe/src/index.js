7987e2f7bb26258917f3d4e94c35c3dbe3cb8953
AlanLazaro <alantsl10@gmail.com>
2022-06-10 09:34:37 -0300

    Mudança da função que marca os quadrados de classe Square para Board

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -3,18 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component{
-    constructor(props){
+  /*  constructor(props){
         super(props);
         this.state = {
             vale: null,
         };
-    }
+    }*/
+
     render(){
         return(
             <button className="square"
-            onClick={() => this.setState({value:'X'})}>
+            onClick={() => this.props.onClick({value:'X'})}>
 
-                {this.state.value}   
+                {this.props.value}   
 
             </button>
         );
@@ -22,9 +23,26 @@ class Square extends React.Component{
 }
 
 class Board extends React.Component{
-    renderSquare(i){
-        return <Square value={i} />;
+    constructor(props){
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
     }
+
+    handleClick(i){
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+    }
+
+    renderSquare(i){
+        return <Square value={this.state.squares[i]} 
+                onClick={() => this.handleClick(i)}
+                />;
+    }
+
+    
     render(){
         const status = 'Next player: X';
         return(
