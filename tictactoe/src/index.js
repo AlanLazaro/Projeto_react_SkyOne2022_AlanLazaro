0484b457c558b2ec3165d700d4d00de50da25fb8
AlanLazaro <alantsl10@gmail.com>
2022-06-10 11:34:46 -0300

    Transformando a classe Square em uma função

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component{
-  /*  constructor(props){
+/*class Square extends React.Component{
+    constructor(props){
         super(props);
         this.state = {
             vale: null,
         };
-    }*/
+    }
 
     render(){
         return(
@@ -20,6 +20,14 @@ class Square extends React.Component{
             </button>
         );
     }
+}*/
+
+function Square(props){
+    return(
+        <button className='square' onClick={props.onClick}>
+            {props.value}
+        </button>
+    )
 }
 
 class Board extends React.Component{
